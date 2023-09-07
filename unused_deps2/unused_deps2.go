2ffe6a1eaa22e3a9d5fb7128569b12b39c3fc012
G Sarath Chandra <gsc.603@gmail.com>
2023-09-07 19:47:16 +0530

    Update unused_deps2.go

--- unused_deps2/unused_deps2.go
+++ unused_deps2/unused_deps2.go
@@ -355,7 +355,7 @@ func main() {
 	}
 	queryCmd = append(queryCmd, blazeFlags...)
 	queryCmd = append(
-		queryCmd, fmt.Sprintf("'kind(\"(kt|java|android)_*\", %s)", strings.Join(targetPatterns, " + ")))
+		queryCmd, fmt.Sprintf("kind('(kt|java|android)_*', %s)", strings.Join(targetPatterns, " + ")))
 	queryCmd = append(queryCmd, "'")
 
 	log.Printf("running: %s %s", *buildTool, strings.Join(queryCmd, " "))
